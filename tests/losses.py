d1427cdd2a38d96089005584dc2f76af6d504f95
masterbatcoderman10 <suhaib.athar@gmail.com>
2024-02-28 10:16:49 +0400

    added CE loss to utils.py

--- tests/losses.py
+++ tests/losses.py
@@ -9,6 +9,20 @@ from torchvision.transforms import functional as F
 from torchvision import transforms
 from torch import nn
 
+class CrossEntropy4D(nn.Module):
+    def __init__(self, class_weights=None):
+        super(CrossEntropy4D, self).__init__()
+        
+        self.cross_entropy_loss = nn.CrossEntropyLoss(weight=class_weights)
+
+    def forward(self, prediction, target):
+        # Reshape predictions and targets to (batch_size * height * width, num_classes)
+        predictions_2d = prediction.permute(0, 2, 3, 1).reshape(-1, prediction.size()[1])
+        targets_2d = target.permute(0, 2, 3, 1).reshape(-1, target.size()[1])
+        loss = self.cross_entropy_loss(predictions_2d, targets_2d.argmax(1))
+        return loss
+
+
 class DiceLoss(nn.Module):
     def __init__(self, num_classes, log_loss=False):
         super(DiceLoss, self).__init__()
